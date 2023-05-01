27bf95aa3e1412a5d4818cceaeaec0ae39dc1072
Feringal <132060719+Feringal@users.noreply.github.com>
2023-05-01 14:20:34 +0200

    Add files via upload

---
+++ test missile/Untitled.lua
@@ -0,0 +1,15 @@
+missile = {}
+missile.x = 20
+missile.y = 20
+missileImg = love.graphics.newImage("images, missile")
+
+function love.load ()
+
+end 
+
+function love.update (dt )
+end 
+
+function love.draw ()
+love.graphics.draw("missileImg, missile.x, missible.y")
+end 
